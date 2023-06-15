52ce61cf2574fe213a3fcb4e6f36826e71e2b648
victoraraujoo <victorm.araujoo@gmail.com>
2023-06-15 10:25:17 -0300

    Add project Checklist with MongoDB integrated

--- 5.0 Checklist MongoDB/app.js
+++ 5.0 Checklist MongoDB/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash")
 
 const app = express();
 
-let itens = [];
-let itensWork = []
-
 mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
-const itensSchema = mongoose.Schema({
+const itemSchema = mongoose.Schema({
     name: String
 });
 
-const Item = mongoose.model("item", itensSchema);
+const listSchema = mongoose.Schema({
+    listName: String,
+    listItens: [itemSchema]
+})
+
+const List = mongoose.model("list", listSchema);
+
+const Item = mongoose.model("item", itemSchema);
 
 app.set("view engine", "ejs");
 
@@ -23,33 +28,77 @@ app.use(express.static("public"))
 
 app.post("/", function(req, res){
 
-    if (req.body.button === "work"){
-        console.log(req.body.newItem)
-        itensWork.push(req.body.newItem);
-        res.redirect("/work");
-    }
-    else{
-    itens.push(req.body.newItem);
-    res.redirect("/");
-    }
+    const newItem = new Item({
+        name: req.body.newItem
+    });
 
+    const title = req.body.button
+
+    if (title === 'day'){ 
+        newItem.save();
+        res.redirect("/");
+    }else{
+        List.findOne({listName: title})
+        .then(function(itens){
+            itens.listItens.push(newItem);
+            itens.save();
+            res.redirect("/" + title);
+        })
+    }
 });
 
-app.get("/work/", function(req,res){
-    res.render("list", {dayOfWeek: "work", newItems: itensWork})
-});
+app.post("/delete", function(req, res){
+    const checkedId = req.body.checkbox
+    const listName1 = req.body.listName
 
+    if (listName1 === "day"){
+        Item.findByIdAndDelete(checkedId)
+        .catch(function(error){
+            console.log(error)
+        })
+        res.redirect("/");
+    }else{
+        List.findOneAndUpdate({listName: listName1}, {$pull: {listItens: {_id : checkedId}}})
+        .then(function(){
+            res.redirect("/" + listName1);
+        })
+        .catch(function(error){-
+            console.log(error)
+        })
+    }
+})
+
+app.get("/:customListName", function(req,res){
+    const listName = _.capitalize(req.params.customListName);
+
+    List.find();
+
+    List.findOne({
+        listName: listName
+    })
+    .then(function(checkedList){                        
+        if (!checkedList){
+            const list = new List({
+                listName: listName,
+                listItens: []
+            })
+             list.save()
+             res.redirect("/" + listName)
+        }else{
+            res.render("list", {listTitle: listName, newItems: checkedList.listItens});
+        }
+    }).catch(function(error){
+        console.log(error)
+    })
+});
 
 app.get("/", function(req, res){
-    let today = new Date();
-    const option = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    let day = today.toLocaleDateString("pt-br", option);
-
-    res.render("list", {dayOfWeek: day, newItems: itens});
+    Item.find()
+    .then(function(itens){
+        res.render("list", {listTitle: "day", newItems: itens});
+    }).catch(function(error){
+        console.log(error)
+    });
 });
 
 
